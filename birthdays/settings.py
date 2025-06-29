3fafcb94fccebc032c42cd45752c2c310d2f960a
SantosKurtMatthew <kurtsantos924@gmail.com>
2025-06-29 20:39:59 +0800

    Updated settings.py to allow hosts

--- birthdays/settings.py
+++ birthdays/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://birthdays-app-8gad3.ondigitalocean.app/"]
 
 
 # Application definition
