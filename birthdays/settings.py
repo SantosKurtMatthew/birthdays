12145815209eec67fb9b2befeee544f314b8ed0c
SantosKurtMatthew <kurtsantos924@gmail.com>
2025-06-29 20:46:05 +0800

    Added static root

--- birthdays/settings.py
+++ birthdays/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [BASE_DIR/'static']
+
 TAILWIND_APP_NAME = 'theme'
 NPM_BIN_PATH = "npm.cmd"
 
